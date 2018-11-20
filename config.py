64b25db78ad846792ef5cf746b0613ab35cb3b7f
Wenfeng <1554484645@qq.com>
2018-11-20 21:24:45 +0800

    add default configuration file written in python

---
+++ config.py
@@ -0,0 +1,11 @@
+CONFIG = {
+    'device': 'cuda',
+    'lr': 1e-1,
+    'batch_size': 128,
+    'network': 'resnet18',
+    'wd': 5e-4,
+    'n_classes': 10,
+    'n_devices': 1,
+    'model_dir': '/tmp/cifar10',
+    'n_epochs': 1,
+}
